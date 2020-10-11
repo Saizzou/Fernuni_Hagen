9ea155a4f2126523c6243e2f779104d462a0d692
Saizzou <65252447+Saizzou@users.noreply.github.com>
2020-10-11 21:50:52 +0200

    Add files via upload
    Fernuni Hagen Einsende Aufgabe 1.4 mit ein par änderungen. Das Program gibt ihnen auch eine meldung ob ein dreieck mit der zahlen erstellt wird d.h. wenn ihr Negative zahlen oder eine "0" eingibt erhält ihr eine fehl meldung das die Eingabe ungültig ist.

---
+++ EA 1.4.pas
@@ -0,0 +1,47 @@
+program pythagoras(input, output);
+
+	var
+	a, b, c : integer;
+	x : boolean;
+	
+begin
+	readln(a);
+	readln(b);
+	readln(c);
+	x:=True;
+	begin
+		if a > 0 then
+			if b > 0 then
+				if c > 0 then
+				writeln ('Dreieck kann berechnet werden')
+			else
+		else
+    else
+    writeln ('Negative zahl ist eingegeben. Bitte nur zahlen grösser als 0 eingeben');
+	end;
+	begin
+		if a > b then
+			if a > c then
+				if sqr(a) = sqr(b) + sqr(c) then
+					x := True
+				else
+					x := False
+			else
+				if sqr(c) = sqr(a) + sqr(b) then
+					x := True
+				else
+					x := False
+		else
+			if b > c then
+				if sqr(b) = sqr(a) + sqr(c) then
+					x := True
+				else
+					x := False    
+			else
+				if sqr(c) = sqr(a) + sqr(b) then
+					x := True
+				else
+					x := False
+	end;
+	writeln(x);
+end.
