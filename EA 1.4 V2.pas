5a18b430dd81bdcf21f28cc87e15866a761e2627
Saizzou <65252447+Saizzou@users.noreply.github.com>
2020-10-11 22:34:49 +0200

    Add files via upload
    Eine andere Version für die EA . Hier wird die funktion "and" benutzt statt nur if - else.

---
+++ EA 1.4 V2.pas
@@ -0,0 +1,42 @@
+program pythagoras(input, output);
+
+	var
+	a, b, c : integer;
+	x : boolean;
+	
+begin
+	readln(a);
+	readln(b);
+	readln(c);
+	begin
+	{ kontrolle ob 0 oder negative zahlen eingegeben sind }
+		if a > 0 then
+			if b > 0 then
+				if c > 0 then
+				writeln ('Dreieck kann berechnet werden')
+			else
+		else
+    else
+    writeln ('Negative zahl ist eingegeben. Bitte nur zahlen grösser als 0 eingeben');
+	end;
+	begin
+	{ die eingegebene werten sortieren }
+		if (a > b) and (a > c) then
+			if sqr(a) = sqr(b) + sqr(c) then
+				x := True
+			else
+				x := False
+		else
+			if (b > a) and (b > c) then
+				if sqr(b) = sqr(a) + sqr(c) then
+					x := True
+				else
+					x := False
+			else
+				if sqr(c) = sqr(a) + sqr(b) then
+					x := True
+				else
+					x := False    
+	end;
+	writeln(x);
+end.
