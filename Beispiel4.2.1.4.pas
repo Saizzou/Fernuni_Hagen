7a79cbe0c0e573d21aa25b47fdb6e326dbffce13
Saizzou <65252447+Saizzou@users.noreply.github.com>
2020-10-27 09:59:31 +0100

    Update Beispiel4.2.1.4.pas

--- Beispiel4.2.1.4.pas
+++ Beispiel4.2.1.4.pas
@@ -7,12 +7,12 @@ integer-Zahlen vorkommt }
 	MAXPLUS1 = 101;
 	
 	type
-	tIndex = 1..MAX;
-	tfeld = array [tIndex] of integer;
-	tIndexPlus1 = 1..MAXPLUS1;
+	tIndex = 1..MAX; { bezeichnung das der Index von 1 bis MAX (wurde in const eingegeben) geht }
+	tfeld = array [tIndex] of integer; { erstellung für eine Liste(Feld) die alle zahlen von tIndex in sich erhaelt }
+	tIndexPlus1 = 1..MAXPLUS1; { MAXPLUS1 ist für dem fall wenn wircklich 100 zahlen eingetippt werden das die while schleife mit 101 enden kann } 
 	
 	var
-	Feld : tFeld;
+	Feld : tFeld; { das Feld kann sich aendern da dieses feld ein inhalt von der for schleife erhalten wird }
 	Groesse,
 	Suchwert : integer;
 	i : tIndexPlus1;
@@ -30,10 +30,10 @@ begin
 	for i := 1 to Groesse do
 	begin
 		write (i, '. Wert:');
-		read (Feld[i])
+		read (Feld[i]) { erstellung einen Feld(Liste) mit zahlen die wir bestimmen }
 	end;
 	write ('Suchwert: ');
-	readln (Suchwert);
+	readln (Suchwert); { suche diese zahl in der vorher eingegebene Liste }
 	
 	{ wir haben die Groesse, dem inhalt und die zahlen wonach
 	wir suchen eingetippt. Jetzt werden wir kontrollieren ob
@@ -54,3 +54,7 @@ begin
 		else
 			writeln ('Der wert befinden sich nicht in der Liste.')
 end. 
+
+{ Falls ihr auch denkt "Warum erstellen wir eine liste und suchen wieder in einer selbst erstellte Liste?" 
+*Ja es macht kein sinn das wir eine eigene Liste erstellen und dann wieder in diese Liste suchen 
+* aber dieser Code ist nur für dem zweck des lernen erstellt wurden. } 
