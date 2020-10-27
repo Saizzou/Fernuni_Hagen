977c8a95c78e38127510de5cd80fe94648d182b1
Saizzou <65252447+Saizzou@users.noreply.github.com>
2020-10-27 09:48:52 +0100

    Update Beispiel4.2.1.6.pas

--- Beispiel4.2.1.6.pas
+++ Beispiel4.2.1.6.pas
@@ -3,12 +3,12 @@ program VokaleUndKonsonanten (input, output);
 vorkommenden Vokale und Konsonanten }
 
 	type
-	tBuchst = 'a'..'z';
-	tNatZahl = 0..maxint;
-	tHaeufigkeit = array [tBuchst] of tNatZahl;
+	tBuchst = 'a'..'z'; { eine Liste mit alle Buchstaben }
+	tNatZahl = 0..maxint; { unser zaehler fur die Liste. Dies dient um die wiederholende buchstaben zu zaehlen }
+	tHaeufigkeit = array [tBuchst] of tNatZahl; { der array dient hier für eine erstellung von einer Liste wie oft der Buchstabe vorkommt }
 	
 	var
-	Anzahl : tHaeufigkeit;
+	Anzahl : tHaeufigkeit; 
 	Zeichen : char;
 	Gesamtzahl,
 	Vokalzahl : tNatZahl;
@@ -24,7 +24,7 @@ begin
 	Aufaddieren der Zaehler }
 	
 	read (Zeichen);
-	while Zeichen <> '.' do
+	while Zeichen <> '.' do { dies ist unsere konfiguration damit unser Program dem Punkt erkennt und bis dahin die buchstaben zaehlt }
 	begin
 		if (Zeichen >= 'a') and (Zeichen <= 'z') then
 		begin	
@@ -36,7 +36,7 @@ begin
 	writeln;
 	
 	Vokalzahl := Anzahl['a'] + Anzahl['e'] + Anzahl['i'] +
-				Anzahl['o'] + Anzahl['u'];
+				Anzahl['o'] + Anzahl['u']; 
 				
 	writeln ('Anzahl der Vokale: ', Vokalzahl, '.');
 	writeln ('Anzahl der Konsonanten: ', Gesamtzahl - Vokalzahl, '.')
